32da3767fb76d9deba88417473d95d7f631120f0
Fernando Doglio <fernando@openreplay.com>
2022-06-09 14:37:19 +0200

    redireecting without re-render hopefully

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,18 +1,29 @@
-import styles from './LoginForm.module.css'
+import styles from "./LoginForm.module.css"
+import Link from 'next/link'
+
+import {useState} from "react"
 
 export default function LoginForm({destination}) {
+  const [email, setEmail] = useState("")
+
+  function updateEmail(evnt) {
+    setEmail(evnt.target.value)
+  }
+
   return (
   <div className={styles.loginBlock}>    
       <h2 className={styles.loginTitle}>Login Page</h2>
       <div className={styles.login}>    
       <form id="login" method="get" action={destination}>    
           <label><b>User Name</b></label>    
-          <input type="text" name="Uname" id="Uname" placeholder="Username" />    
+          <input type="text" name="Uname" id="Uname" placeholder="Username" onKeyUp={updateEmail}/>    
           <br /><br/>    
           <label><b>Password</b></label>    
           <input type="Password" name="Pass" id="Pass" placeholder="Password" />    
           <br /><br/>    
-          <input type="submit" name="log" id="log" value="Log In Here" />       
+          <Link href={{
+            pathname: "/logged-in",
+            query: {Uname: email}}} ><a>Log In Here</a></Link>
       </form>     
     </div>    
     </div>
